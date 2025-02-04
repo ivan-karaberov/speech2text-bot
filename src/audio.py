c20e07e649924e9691327fb6bcf2078280a88d1f
ivan-karaberov <karaberov.ivan@yandex.ru>
2025-02-04 21:45:53 +0300

    feat: add transcribed_audio

---
+++ src/audio.py
@@ -0,0 +1,28 @@
+import logging
+
+from faster_whisper import WhisperModel
+
+log = logging.getLogger(__name__)
+
+
+class Audio:
+    def __init__(
+        self,
+        *,
+        model_size: str = "small",
+        device: str = "cpu",
+        compute_type: str = "int8"
+    ) -> None:
+        self.model = WhisperModel(
+            model_size_or_path=model_size,
+            device=device,
+            compute_type=compute_type
+        )
+
+    def transcribe_audio(self, audio_file: str) -> str | None:
+        try:
+            segments, _ = self.model.transcribe(audio_file)
+            return "".join([segment.text for segment in segments])
+        except Exception as e:
+            log.error("Error during transcription audio: %s", e)
+            return None
